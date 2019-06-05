778023f4ead3f6fe1664e8ee120ee1eecf11e1b1
Michael Snowden <mtsnowden@gmail.com>
2019-06-04 20:19:40 -0500

    starting project

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+
+app.get('/users', (req, res) => res.json(users))
+
+app.get('/users/1', (req, res) => res.json(users))
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
