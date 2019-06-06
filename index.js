460f0ba48a81e9e714c894145a91e91b6fff21a8
Michael Snowden <mtsnowden@gmail.com>
2019-06-06 16:55:49 -0500

    get post delete users complete

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 const bodyParser = require('body-parser')
-const state = require ('./state')
+const { users } = require ('./state')
 
 
 app.use(bodyParser.json()); 
 
 app.get('/', (req, res) => res.send('hello world'))
 
-// app.get('/users/1', (req, res) => res.json(users))
+app.get('/users', (req, res) => res.json(users))
+
+app.get('/users/1', (req, res) => res.json(users[0]))
+
+app.post('/users', (req, res) => {
+  const newUser = {
+    name: 'Barrett',
+    occupation: 'Teacher',
+    age: 32
+  }
+  users.push(newUser)
+  res.json(users[users.length-1])
+})
+
+app.put('/users', (req, res) => {
+  const userEdit = users[0]
+  userEdit.name = 'Sally'
+  res.json(users[0])
+})
+
+app.delete('/users/1', (req, res) => {
+  users.shift()
+  res.send('deleted')
+})
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
