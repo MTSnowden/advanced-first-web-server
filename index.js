99f2ff11e6a393ae535aa9f6d2787b266d342869
Michael Snowden <mtsnowden@gmail.com>
2019-06-06 15:51:34 -0500

    got hello world to print finally

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const bodyParser = require('body-parser')
+const state = require ('./state')
 
-app.get('/users', (req, res) => res.json(users))
 
-app.get('/users/1', (req, res) => res.json(users))
+app.use(bodyParser.json()); 
+
+app.get('/', (req, res) => res.send('hello world'))
+
+// app.get('/users/1', (req, res) => res.json(users))
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
+
