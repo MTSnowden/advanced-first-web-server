5631e32e797c4442fa2a45e8882316b2b13ed853
Michael Snowden <mtsnowden@gmail.com>
2019-06-06 19:42:20 -0500

    cant figure out the last part

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
-const bodyParser = require('body-parser')
 const { users } = require ('./state')
 
 
@@ -13,15 +13,15 @@ app.get('/users', (req, res) => res.json(users))
 
 app.get('/users/1', (req, res) => res.json(users[0]))
 
-app.post('/users', (req, res) => {
-  const newUser = {
-    name: 'Barrett',
-    occupation: 'Teacher',
-    age: 32
-  }
-  users.push(newUser)
-  res.json(users[users.length-1])
-})
+// app.post('/users', (req, res) => {
+//   const newUser = {
+//     name: 'Barrett',
+//     occupation: 'Teacher',
+//     age: 32
+//   }
+//   users.push(newUser)
+//   res.json(users[users.length-1])
+// })
 
 app.put('/users', (req, res) => {
   const userEdit = users[0]
@@ -33,6 +33,17 @@ app.delete('/users/1', (req, res) => {
   users.shift()
   res.send('deleted')
 })
+
+app.post('/users', (req, res) => {
+  const newUser = req.body
+  const counter = users.length+1
+  newUser._id = counter
+  console.log(newUser)
+  users.push(newUser)
+  res.json(newUser)
+
+})
+
 
 app.listen(port, () => 
   console.log(`Example app listening on port ${port}!`))
